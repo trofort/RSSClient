5e4b68c412712930af36a25e47452b575b6efc7c
Максим Деханов <Maksim.dehanov@life.com.by>
2020-08-25 12:52:10 +0300

    Added Share news

--- lib/Screens/NewsWebViewScreen/NewsWebViewScreen.dart
+++ lib/Screens/NewsWebViewScreen/NewsWebViewScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:rss_client/Models/RSSNewsItemModel.dart';
+import 'package:share/share.dart';
 
 class NewsWebViewScreen extends StatelessWidget {
 
@@ -11,6 +12,13 @@ class NewsWebViewScreen extends StatelessWidget {
       url: newsItem.link,
       appBar: AppBar(
         title: Text(newsItem.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            iconSize: 24.0,
+            onPressed: () => Share.share("Check this news: ${newsItem.link}"),
+          )
+        ],
       ),
     );
   }
