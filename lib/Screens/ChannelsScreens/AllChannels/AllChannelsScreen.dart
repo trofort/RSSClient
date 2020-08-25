0c131cc77a1863c12d2def3dc9de5c7eaa50a5d0
Максим Деханов <Maksim.dehanov@life.com.by>
2020-08-25 14:26:19 +0300

    Added Pull to Refresh

--- lib/Screens/ChannelsScreens/AllChannels/AllChannelsScreen.dart
+++ lib/Screens/ChannelsScreens/AllChannels/AllChannelsScreen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:http/http.dart' as NetworkService;
 import 'package:rss_client/Models/RSSChannelModel.dart';
 import 'package:rss_client/Models/RSSNewsItemModel.dart';
 import 'package:rss_client/Screens/ChannelsScreens/Cells/NewsItemCell.dart';
 import 'package:rss_client/Screens/ChannelsScreens/Controller/ChannelController.dart';
 import 'package:rss_client/Services/DataBase/Storages/ChannelStorage.dart';
-import 'package:rss_client/Services/DataBase/Storages/NewsItemStorage.dart';
-import 'package:rss_client/Services/HTMLService.dart';
 
-class AllChannelsScreen extends StatelessWidget {
+class AllChannelsScreen extends StatefulWidget {
+
+  createState() => _AllChannelsScreenState();
+
+}
+
+class _AllChannelsScreenState extends State<AllChannelsScreen> {
 
   final ChannelController _controller = ChannelController();
 
@@ -20,32 +23,35 @@ class AllChannelsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('All')
       ),
-      body: FutureBuilder(
-        future: ChannelStorage.getAll(),
-        builder: (BuildContext context, AsyncSnapshot<List<RSSChannelModel>> snapshot) {
-          if (snapshot.hasData) {
-            final allChannels = snapshot.data;
-            return FutureBuilder(
-              future: Future.wait(allChannels.map((e) => _controller.getContent(e))),
-              builder: (BuildContext context, AsyncSnapshot<List<List<RSSNewsItemModel>>> snapshot) {
-                if (snapshot.hasData) {
-                  final allNews = snapshot.data
-                    .expand((element) => element)
-                    .toList();
-                  allNews.sort((a, b) => b.pubDate.compareTo(a.pubDate));
-                  return ListView(
-                    children: allNews.map((e) => NewsItemCell(item: e,)).toList(),
-                  );
-                } else {
-                  return _waveLoader();
-                }
-              },
-            );
-          } else {
-            return _waveLoader();
-          }
-        },
-      ),
+      body: RefreshIndicator(
+        onRefresh: () async { setState(() {}); },
+        child: FutureBuilder(
+          future: ChannelStorage.getAll(),
+          builder: (BuildContext context, AsyncSnapshot<List<RSSChannelModel>> snapshot) {
+            if (snapshot.hasData) {
+              final allChannels = snapshot.data;
+              return FutureBuilder(
+                future: Future.wait(allChannels.map((e) => _controller.getContent(e))),
+                builder: (BuildContext context, AsyncSnapshot<List<List<RSSNewsItemModel>>> snapshot) {
+                  if (snapshot.hasData) {
+                    final allNews = snapshot.data
+                        .expand((element) => element)
+                        .toList();
+                    allNews.sort((a, b) => b.pubDate.compareTo(a.pubDate));
+                    return ListView(
+                      children: allNews.map((e) => NewsItemCell(item: e,)).toList(),
+                    );
+                  } else {
+                    return _waveLoader();
+                  }
+                },
+              );
+            } else {
+              return _waveLoader();
+            }
+          },
+        ),
+      )
     );
   }
 
--- lib/Screens/ChannelsScreens/Channel/ChannelScreen.dart
+++ lib/Screens/ChannelsScreens/Channel/ChannelScreen.dart
@@ -7,8 +7,11 @@ import 'package:rss_client/Models/RSSNewsItemModel.dart';
 import 'package:rss_client/Screens/ChannelsScreens/Controller/ChannelController.dart';
 import '../Cells/NewsItemCell.dart';
 
+class ChannelScreen extends StatefulWidget {
+  createState() => _ChannelScreenState();
+}
 
-class ChannelScreen extends StatelessWidget {
+class _ChannelScreenState extends State<ChannelScreen> {
 
   ChannelController _controller = ChannelController();
 
@@ -16,72 +19,75 @@ class ChannelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     _controller.context = context;
     RSSChannelModel channel = ModalRoute.of(context).settings.arguments;
-    return Container(
-      color: Colors.white,
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 250.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(channel.title),
-              background: Hero(
-                tag: channel.source,
-                child: CachedNetworkImage(
-                  placeholder: (context, url) {
-                    if (url.isNotEmpty) {
-                      return SpinKitRotatingPlain(
-                        color: Colors.black12,
-                      );
-                    } else {
-                      return Align(
-                          alignment: Alignment.center,
-                          child: AutoSizeText(
-                            Uri.parse(channel.source).host,
-                            maxLines: 1,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                              fontSize: 32.0,
-                              decoration: TextDecoration.none,
-                              fontFamily: '.SF UI Text'
+    return RefreshIndicator(
+      onRefresh: () async { setState(() {}); },
+      child: Container(
+        color: Colors.white,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 250.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(channel.title),
+                background: Hero(
+                    tag: channel.source,
+                    child: CachedNetworkImage(
+                      placeholder: (context, url) {
+                        if (url.isNotEmpty) {
+                          return SpinKitRotatingPlain(
+                            color: Colors.black12,
+                          );
+                        } else {
+                          return Align(
+                            alignment: Alignment.center,
+                            child: AutoSizeText(
+                              Uri.parse(channel.source).host,
+                              maxLines: 1,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 32.0,
+                                  decoration: TextDecoration.none,
+                                  fontFamily: '.SF UI Text'
+                              ),
                             ),
-                          ),
-                      );
-                    }
-                  },
-                  imageUrl: channel.imageUrl,
-                  fit: BoxFit.fitWidth,
-                  height: double.infinity,
-                  width: double.infinity,
-                )
+                          );
+                        }
+                      },
+                      imageUrl: channel.imageUrl,
+                      fit: BoxFit.fitWidth,
+                      height: double.infinity,
+                      width: double.infinity,
+                    )
+                ),
               ),
             ),
-          ),
-          FutureBuilder(
-            future: _controller.getContent(channel),
-            builder: (BuildContext context, AsyncSnapshot<List<RSSNewsItemModel>> snapshot) {
-              if (snapshot.hasData) {
-                List<RSSNewsItemModel> news = snapshot.data;
-                news.sort((a, b) => b.pubDate.compareTo(a.pubDate));
-                return SliverList(
-                  delegate: SliverChildListDelegate(
-                      news.map((item) => NewsItemCell(item: item)).toList()
-                  ),
-                );
-              } else {
-                return SliverToBoxAdapter(
-                  child: Container(
-                    padding: EdgeInsets.all(24.0),
-                    child: SpinKitWave(
-                      color: Colors.black,
+            FutureBuilder(
+              future: _controller.getContent(channel),
+              builder: (BuildContext context, AsyncSnapshot<List<RSSNewsItemModel>> snapshot) {
+                if (snapshot.hasData) {
+                  List<RSSNewsItemModel> news = snapshot.data;
+                  news.sort((a, b) => b.pubDate.compareTo(a.pubDate));
+                  return SliverList(
+                    delegate: SliverChildListDelegate(
+                        news.map((item) => NewsItemCell(item: item)).toList()
                     ),
-                  )
-                );
-              }
-            },
-          )
-        ],
+                  );
+                } else {
+                  return SliverToBoxAdapter(
+                      child: Container(
+                        padding: EdgeInsets.all(24.0),
+                        child: SpinKitWave(
+                          color: Colors.black,
+                        ),
+                      )
+                  );
+                }
+              },
+            )
+          ],
+        ),
       ),
     );
   }
